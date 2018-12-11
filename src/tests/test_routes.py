5ddb0d62b014768c07cd350dec1eb7203cc012d4
mthwbrwn <mthwbrwn@gmail.com>
2018-12-11 14:25:39 -0800

    home routes passe

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,6 +1,12 @@
 from .. import app
 
+
 def test_home_route_get():
     rv = app.test_client().get('/')
     assert rv.status_code == 200
     assert b'<h1>Welcome to the site</h1>' in rv.data
+
+
+def test_home_route_delete():
+    rv = app.test_client().delete('/')
+    assert rv.status_code == 405
