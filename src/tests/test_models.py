7df2504abccf38a82bfef5d247e7684d239f47b4
mthwbrwn <mthwbrwn@gmail.com>
2018-12-11 00:05:10 -0800

    turning in what I have

--- src/tests/test_models.py
+++ src/tests/test_models.py
@@ -1,19 +1,2 @@
 import pytest
-from .. models import db, Company
 
-@pytest.fixture
-def session():
-
-    def do_nothing():
-        pass
-
-    db.session.commit = do_nothing
-
-    yield db.session
-
-    db.session.rollback()
-
-
-def test_company_all(session):
-    companies = Company.query.all()
-    assert len(companies) == 0
