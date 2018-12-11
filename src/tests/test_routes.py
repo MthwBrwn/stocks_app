d448a8dbe4b9508df2f4892a1e1f5c6cbc7be740
mthwbrwn <mthwbrwn@gmail.com>
2018-12-11 14:23:06 -0800

    home routes passe

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,4 +1,6 @@
 from .. import app
 
-def test_home_route():
-    assert app
+def test_home_route_get():
+    rv = app.test_client().get('/')
+    assert rv.status_code == 200
+    assert b'<h1>Welcome to the site</h1>' in rv.data
