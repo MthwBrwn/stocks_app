ec99eac0bff72ffec4173584ecaa4003560ca399
mthwbrwn <mthwbrwn@gmail.com>
2018-12-24 14:52:02 -0800

    new models added

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import sha256_crypt
 from datetime import datetime as dt
 from flask_migrate import Migrate
 from . import app
-# from passlib.hash import sha256_crypt
 
 
 db = SQLAlchemy(app)
@@ -35,15 +35,7 @@ class Portfolio(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True)
-    # symbol = db.Column(db.String(64), index=True, unique=True)
-    # companyName = db.Column(db.String(256), index=True, unique=True)
-    # exchange = db.Column(db.String(128))
-    # industry = db.Column(db.String(128))
-    # website = db.Column(db.String(128))
-    # description = db.Column(db.Text)
-    # CEO = db.Column(db.String(128))
-    # issueType = db.Column(db.String(128))
-    # sector = db.Column(db.String(128))
+
 
     companies = db.relationship('Company', backref='portfolio', lazy=True)
 
@@ -51,13 +43,30 @@ class Portfolio(db.Model):
 
     def __repr__(self):
         return '< Portfolio : {} >'.format(self. name)
-    # do things
-    # def __init__(self, email, password):
-    #     self.email = email
-    #     self.password = sha256_crypt.encrypt(password)
 
-# @classmethod
-# def check_password_hash(cls, user password):
-#     """
-#     """
-#     if sha256_ rypt.verify(password, user.password):
+
+class User(db.Model):
+    """
+    """
+     __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), index=True, nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+    portfoliios = db.relationship('Portfolio', backref='user', lazy=True)
+
+    def __repr__(self):
+        return '< User : {} >'.format(self. email)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = sha256_crypt.encrypt(password)
+        #come back here
+
+    @classmethod
+    def check_password_hash(cls, user, password):
+        """
+        """
+        if use is not None
+        if sha256_ rypt.verify(password, user.password):
